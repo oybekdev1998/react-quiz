16354e3cdd1b57565b638e0d6ba9c521f3ccd83e
oybekdev1998 <oybekjon.shamuratov@mail.ru>
2022-01-27 17:21:46 +0500

    Navigation

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react'
 import Layout from './hoc/Layout/Layout'
-import {Route, Switch} from 'react-router-dom'
+import {Route, Routes} from 'react-router-dom'
 import Quiz from './containers/Quiz/Quiz'
 import Auth from './containers/Auth/Auth'
 import QuizCreator from './containers/QuizCreater/QuizCreator'
@@ -11,12 +11,12 @@ class App extends Component {
   render() {
     return (
       <Layout>
-        <Switch>
-          <Route path="/auth" component={Auth}/>
-          <Route path="/quiz-creator" component={QuizCreator}/>
-          <Route path="/quiz/:id" component={QuizList}/>
-          <Route path="/" component={Quiz}/>
-        </Switch>
+        <Routes>
+          <Route path="/auth" element={<Auth/>}/>
+          <Route path="/quiz-creator" element={<QuizCreator/>}/>
+          <Route path="/quiz/:id" element={<QuizList/>}/>
+          <Route path="/" element={<Quiz/>}/>
+        </Routes>
       </Layout>
     )
   }
